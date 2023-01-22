983ec5d32f7772997290a54a5f40f305e19097a7
kh0w <savov.130+github@gmail.com>
2023-01-22 12:57:19 +0200

    added everything 👍

---
+++ chat.js
@@ -0,0 +1,12 @@
+//JavaScript code to handle sending and receiving messages
+
+//Get necessary DOM elements
+const messageInput = document.getElementById(message-input);
+const sendBtn = document.getElementById(send-btn);
+const messageList = document.getElementById(message-list);
+
+//Handle form submit event
+document.querySelector(form).addEventListener(submit, e = {
+  e.preventDefault();
+  //Get message text
+  const message = messageInput.
